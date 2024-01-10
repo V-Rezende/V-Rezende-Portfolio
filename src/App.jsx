e27d47907c8e13a2a22c76ce23ad139697bcf5d3
Vinni Rezende <vinnirezende1980@gmail.com>
2024-01-10 15:19:28 +1000

    last commit 10-01

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <body>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route exact path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/projects' element={<Projects/>} />
         <Route path='/contact' element={<Contact/>} />
